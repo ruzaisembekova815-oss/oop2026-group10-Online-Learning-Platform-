4704985a1ef563fd0fe0324bef5bc41770afd838
kapchaman <69698403+kapchaman@users.noreply.github.com>
2026-01-20 17:40:57 +0500

    2

--- src/edu/aitu/oop3/db/entity/course.java
+++ src/edu/aitu/oop3/db/entity/course.java
@@ -1,31 +1,34 @@
 package edu.aitu.oop3.db.entity;
 
 public class course {
-        private int id;
-        private String title;
-        private String description;
-        private int instructorId;
-        private boolean archived;
 
-        public course(int id, String title, String description, int instructorId, boolean archived) {
-            this.id = id;
-            this.title = title;
-            this.description = description;
-            this.instructorId = instructorId;
-            this.archived = archived;
-        }
+    private int id;
+    private String title;
+    private String description;
+    private int instructorId;
+    private boolean archived;
 
-        public int getId() {
-            return id;
-        }
+    public course(int id, String title, String description, int instructorId, boolean archived) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.instructorId = instructorId;
+        this.archived = archived;
+    }
 
-        public boolean isArchived() {
-            return archived;
-        }
+    public int getId() {
+        return id;
+    }
 
-        public String getTitle() {
-            return title;
-        }
+    public String getTitle() {
+        return title;
+    }
 
+    public boolean isArchived() {
+        return archived;
+    }
 
+    public int getInstructorId() {
+        return instructorId;
+    }
 }
--- src/edu/aitu/oop3/db/extension/AppDemo.java
+++ src/edu/aitu/oop3/db/extension/AppDemo.java
@@ -1,4 +1,37 @@
 package edu.aitu.oop3.db.extension;
 
+import edu.aitu.oop3.db.entity.course;
+import edu.aitu.oop3.db.entity.lesson;
+import edu.aitu.oop3.db.entity.user;
+import edu.aitu.oop3.db.service.CourseService;
+import edu.aitu.oop3.db.service.LessonService;
+import edu.aitu.oop3.db.service.UserService;
+
 public class AppDemo {
+
+    public static void main(String[] args) {
+
+        UserService userService = new UserService();
+        CourseService courseService = new CourseService();
+        LessonService lessonService = new LessonService();
+
+        System.out.println("=== USERS ===");
+        for (user u : userService.getAllUsers()) {
+            System.out.println(u.getId() + " | " + u.getName() + " | " + u.getRole());
+        }
+
+        course java = new course(1, "Java OOP", "Learn OOP in Java", 1, false);
+        courseService.addCourse(java);
+
+        lessonService.addLesson(new lesson(1, 1, "Classes & Objects"));
+        lessonService.addLesson(new lesson(2, 1, "Inheritance"));
+
+        System.out.println("\n=== COURSES ===");
+        courseService.getActiveCourses()
+                .forEach(c -> System.out.println(c.getTitle()));
+
+        System.out.println("\n=== LESSONS FOR COURSE 1 ===");
+        lessonService.getLessonsByCourse(1)
+                .forEach(l -> System.out.println(l.getTitle()));
+    }
 }
